33dadc0cc8607c3d9b427f9b1402434f08ad9738
megascatterbomb <megascatterbomb@gmail.com>
2024-08-25 19:05:29 +1200

    memory/writes optimization

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{borrow::BorrowMut, env, fs};
+use std::{borrow::BorrowMut, env, fs::{self, File}};
 use anyhow::Error;
 use std::io::Write;
 use serde_json::Value;
@@ -34,7 +34,7 @@ fn perform_tick(ticker: &mut DemoTicker<GameStateAnalyser>) {
 
     let mut state_history: Vec<serde_json::Value> = Vec::new();
 
-    let mut file = match fs::File::create("./test/test.wip") {
+    let mut file = match fs::File::create("./test/test.json") {
         Ok(file) => file,
         Err(err) => {
             if err.kind() != std::io::ErrorKind::AlreadyExists {
@@ -47,9 +47,9 @@ fn perform_tick(ticker: &mut DemoTicker<GameStateAnalyser>) {
 
     let mut ticknum: u32 = 0;
 
-    let max_states_in_memory = 8 * 1024;
+    let max_states_in_memory = 1024;
 
-    writeln!(file, "[").unwrap();
+    write!(file, "[").unwrap();
 
     while ticker_result.is_ok_and(|b| b) { 
 
@@ -60,22 +60,14 @@ fn perform_tick(ticker: &mut DemoTicker<GameStateAnalyser>) {
         // Get the GameState from the parser
 
         let state: &GameState = ticker.state();
-        let mut json_value: Value = serde_json::to_value(state).unwrap();
-        let json_object = json_value.as_object_mut().unwrap();
-        json_object.insert("tick".to_string(), serde_json::Value::String(ticknum.to_string()));
-        json_object.remove("kills");
-        state_history.push(json_value);
+        let mut json = get_gamestate_json(state);
+        json = modify_json(&mut json, ticknum);
+
+        state_history.push(json);
 
         if state_history.len() > max_states_in_memory {
-            let mut states_remaining = state_history.len();
-            for json_object in &state_history {
-                if states_remaining > 0 && (states_remaining & (states_remaining - 1) == 0 || states_remaining % 1024 == 0) {
-                    println!("Flushing to disk ({} states remaining)", states_remaining);
-                }
-                write!(file, "{}", serde_json::to_string(&json_object).unwrap()).unwrap();
-                writeln!(file, ",").unwrap();
-                states_remaining -= 1;
-            }
+            write_states_to_file(&mut file, &state_history);
+
             state_history.clear();
         }
 
@@ -86,15 +78,9 @@ fn perform_tick(ticker: &mut DemoTicker<GameStateAnalyser>) {
     }
 
     if state_history.len() > 0 {
-        let mut states_remaining = state_history.len();
-        for json_object in state_history {
-            if states_remaining > 0 && (states_remaining & (states_remaining - 1) == 0 || states_remaining % 1024 == 0) {
-                println!("Flushing to disk ({} states remaining)", states_remaining);
-            }
-            write!(file, "{}", serde_json::to_string(&json_object).unwrap()).unwrap();
-            writeln!(file, ",").unwrap();
-            states_remaining -= 1;
-        }
+        write_states_to_file(&mut file,&state_history);
+
+        state_history.clear();
     }
 
     writeln!(file, "]").unwrap();
@@ -102,4 +88,34 @@ fn perform_tick(ticker: &mut DemoTicker<GameStateAnalyser>) {
     let _ = file.flush();
 
     println!("Done!");
+}
+
+fn get_gamestate_json(state: &GameState) -> Value {
+    serde_json::to_value(state).unwrap()
+}
+
+fn modify_json(state_json: &mut Value, ticknum: u32) -> Value {
+    let json_object = state_json.as_object_mut().unwrap();
+
+    // Insert tick number
+    json_object.insert("tick".to_string(), serde_json::Value::String(ticknum.to_string()));
+
+    // Remove kills as it is cumulative (only need latest value)
+    json_object.remove("kills");
+
+    json_object.entry("players".to_string()).and_modify(|v| {
+        let players = v.as_array_mut().unwrap();
+        *players = players.iter().filter(|p| p["in_pvs"].as_bool().unwrap()).cloned().collect();
+    });
+
+    return serde_json::to_value(json_object).unwrap();
+
+}
+
+fn write_states_to_file(file: &mut File, states: &Vec<Value>) {
+    let out = states.iter()
+                .map(|j| serde_json::to_string(&j).unwrap())
+                .collect::<Vec<String>>().join(",\n") + ",\n";
+
+    writeln!(file, "{}", out).unwrap();
 }
